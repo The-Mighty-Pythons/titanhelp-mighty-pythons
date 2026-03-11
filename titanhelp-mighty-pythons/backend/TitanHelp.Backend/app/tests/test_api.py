cb7ad727f1ebdce3e45ae376474d953d6122a8e2
Amanda Crotty <fullstackmandurlorian@gmail.com>
2026-03-11 12:55:21 -0400

    get tickets test

--- titanhelp-mighty-pythons/backend/TitanHelp.Backend/app/tests/test_api.py
+++ titanhelp-mighty-pythons/backend/TitanHelp.Backend/app/tests/test_api.py
@@ -1,3 +1,11 @@
-def test_get_tickets(client):
+import json
+
+
+def test_get_tickets_returns_200(client):
     response = client.get("/api/tickets")
     assert response.status_code == 200
+
+def test_get_tickets_returns_list(client):
+    response = client.get("/api/tickets")
+    data = response.get_json()
+    assert isinstance(data, list)
